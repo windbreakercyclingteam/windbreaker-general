79ab86652460cf6619402a86f9f7ea24633078a5
windbreakercyclingteam <windbreakercyclingteam@gmail.com>
2025-12-31 18:26:39 -0500

    Resolve merge conflicts

--- app/[...page]/page.tsx
+++ app/[...page]/page.tsx
@@ -1,25 +1,74 @@
-import { builder } from "@builder.io/sdk";
-import { BuilderComponent } from "@builder.io/react";
+// pages/[...page].tsx
+import React from "react";
+import { useRouter } from "next/router";
+import { BuilderComponent, builder, useIsPreviewing } from "@builder.io/react";
+import { BuilderContent } from "@builder.io/sdk";
+import DefaultErrorPage from "next/error";
+import Head from "next/head";
+import { GetStaticProps } from "next";
 
-builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+// Replace with your Public API Key
+builder.init("967b3d7bc2694463b2238f4855406fac");
 
-interface PageProps {
-  params: {
-    page: string[];
-  };
-}
-
-export default async function Page(props: PageProps) {
-  const urlPath = "/" + (props.params?.page?.join("/") || "");
-  
-  const content = await builder
-    .get("page", {
+// Define a function that fetches the Builder
+// content for a given page
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  // Fetch the builder content for the given page
+  const page = await builder
+    .get("windbreaker", {
       userAttributes: {
-        urlPath,
+        urlPath: "/" + ((params?.page as string[])?.join("/") || ""),
       },
     })
     .toPromise();
 
-  return <BuilderComponent content={content} model="page" />;
+  // Return the page content as props
+  return {
+    props: {
+      page: page || null,
+    },
+    // Revalidate the content every 5 seconds
+    revalidate: 5,
+  };
+};
+
+// Define a function that generates the
+// static paths for all pages in Builder
+export async function getStaticPaths() {
+  // Get a list of all pages in Builder
+  const pages = await builder.getAll("windbreaker", {
+    // We only need the URL field
+    fields: "data.url",
+    options: { noTargeting: true },
+  });
+
+  // Generate the static paths for all pages in Builder
+  return {
+    paths: pages.map((page) => page.data?.url).filter(url => url !== '/'),
+    fallback: 'blocking',
+  };
 }
 
+// Define the Page component
+export default function Page({ page }: { page: BuilderContent | null }) {
+  const router = useRouter();
+  const isPreviewing = useIsPreviewing();
+
+  // If the page content is not available
+  // and not in preview mode, show a 404 error page
+  if (!page && !isPreviewing) {
+    return <DefaultErrorPage statusCode={404} />;
+  }
+
+  // If the page content is available, render
+  // the BuilderComponent with the page content
+  return (
+    <>
+      <Head>
+        <title>{page?.data?.title}</title>
+      </Head>
+      {/* Render the Builder page */}
+      <BuilderComponent model="windbreaker" content={page || undefined} />
+    </>
+  );
+}
