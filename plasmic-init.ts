025a8107bb119d0170ee147eaa8e47b9e9fd7cc7
windbreakercyclingteam <windbreakercyclingteam@gmail.com>
2026-01-15 21:39:05 -0500

    Connect Plasmic + Next.js + Appwrite

---
+++ plasmic-init.ts
@@ -0,0 +1,11 @@
+import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+
+export const PLASMIC = initPlasmicLoader({
+  projects: [
+    {
+      id: "gDcrBQgUDAPSPX7kSZtPse",
+      token: "YOUR_NEW_TOKEN_HERE", 
+    },
+  ],
+  
+});
